95a8f3a8d131a6f4303ce4265d41bd7a55de1b1e
NileshCh1 <142974187+NileshCh1@users.noreply.github.com>
2023-12-26 18:18:35 +0530

    Update cartUtility.js

--- cartUtility.js
+++ cartUtility.js
@@ -1,90 +1,81 @@
 import UserUtility from "./userUtility.js";
+import { Cart } from "./models.js";
 
-// Cart class to manage the items in the user's shopping cart
-class cartUtility {
-  constructor(userId) {
-    this.userId = userId;
-    this.items = this.loadCartFromSession();
-  }
-
-  addItem(product, quantity = 1) {
-
-    //get current user check if it's not null and get the id
+class CartUtility {
+  static addItemToCart(product, quantity = 1) {
     const userUtility = new UserUtility();
     const currentUser = userUtility.getCurrentUser();
+
     if (currentUser == null) {
-        alert("User not logged in. Please log in to use the shopping cart.");
-        return;
-    }
-    this.userId = currentUser.id;
-    this.items = this.loadCartFromSession();
-    const existingItem = this.items.find(
-      (item) => item.product.id === product.id
-    );
-
-    if (existingItem) {
-      existingItem.quantity += quantity;
-    } else {
-      this.items.push({ product, quantity });
+      alert("User not logged in. Please log in to use the shopping cart.");
+      return;
     }
 
-    this.updateSession();
+    const userId = currentUser.id;
+    const cart = new Cart(userId);
+
+    cart.addItem(product, quantity);
   }
 
-  removeItem(productId) {
-    this.items = this.items.filter((item) => item.product.id !== productId);
-    this.updateSession();
-  }
+  static removeItemFromCart(productId) {
+    const userUtility = new UserUtility();
+    const currentUser = userUtility.getCurrentUser();
 
-  getItems() {
-    return this.items;
-  }
-
-  getTotal() {
-    return this.items.reduce(
-      (total, item) => total + item.product.price * item.quantity,
-      0
-    );
-  }
-
-  checkout(paymentMethod) {
-    // Perform checkout logic, such as processing the payment
-    paymentMethod.processPayment(this.getTotal());
-    // Clear the cart after successful checkout
-    this.items = [];
-    this.updateSession();
-  }
-
-  updateSession() {
-    // Use the userId to store the cart data per user
-    window.sessionStorage.setItem(
-      `cart_${this.userId}`,
-      JSON.stringify(this.items)
-    );
-  }
-
-  loadCartFromSession() {
-    // Use the userId to retrieve the cart data per user
-    const storedCart = window.sessionStorage.getItem(`cart_${this.userId}`);
-
-    //check if storedCart is null, if so, create an empty cart
-
-    if (!storedCart) {
-      return Cart.createEmptyCart(this.userId); // Use Cart.createEmptyCart
+    if (currentUser == null) {
+      alert("User not logged in. Please log in to use the shopping cart.");
+      return;
     }
 
-    return storedCart;
+    const userId = currentUser.id;
+    const cart = new Cart(userId);
+
+    cart.removeItem(productId);
   }
 
-  static createEmptyCart(userId) {
-    // Create an empty cart for the user
-    const emptyCart = { userId, items: [] };
-    window.sessionStorage.setItem(
-      `cart_${userId}`,
-      JSON.stringify(emptyCart.items)
-    );
-    return emptyCart;
+  static getCartItems() {
+    const userUtility = new UserUtility();
+    const currentUser = userUtility.getCurrentUser();
+
+    if (currentUser == null) {
+      alert("User not logged in. Please log in to use the shopping cart.");
+      return [];
+    }
+
+    const userId = currentUser.id;
+    const cart = new Cart(userId);
+
+    return cart.getItems();
+  }
+
+  static getCartTotal() {
+    const userUtility = new UserUtility();
+    const currentUser = userUtility.getCurrentUser();
+
+    if (currentUser == null) {
+      alert("User not logged in. Please log in to use the shopping cart.");
+      return 0;
+    }
+
+    const userId = currentUser.id;
+    const cart = new Cart(userId);
+
+    return cart.getTotal();
+  }
+
+  static checkoutCart(paymentMethod) {
+    const userUtility = new UserUtility();
+    const currentUser = userUtility.getCurrentUser();
+
+    if (currentUser == null) {
+      alert("User not logged in. Please log in to use the shopping cart.");
+      return;
+    }
+
+    const userId = currentUser.id;
+    const cart = new Cart(userId);
+
+    cart.checkout(paymentMethod);
   }
 }
 
-export default cartUtility;
+export default CartUtility;

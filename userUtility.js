f3bf652395bdafdf5c26a2441d8b3214a7caac56
NileshCh1 <142974187+NileshCh1@users.noreply.github.com>
2023-12-26 18:23:19 +0530

    Update userUtility.js

--- userUtility.js
+++ userUtility.js
@@ -3,46 +3,58 @@ import User from './userModel.js';
 class UserUtility {
   constructor() {
     this.users = [];
+    this.currentUser = null;
   }
 
   addUser(user) {
     this.users.push(user);
+    this.setCurrentUser(user);
     this.updateSession();
   }
 
   findUserByEmail(email) {
-   // return this.users.find(user => user.getEmail() === email);\
     return this.users.find(user => user.email === email);
-
   }
 
-  findUserByName (name) {
-
-    //print (this.users);
-
-    console.log(this.users);
-
+  findUserByName(name) {
     return this.users.find(user => user.id === name);
   }
 
   validatePassword(user, inputPassword) {
-    //validate password from user object 
-    
-    if(user&& user.password === inputPassword){
+    if (user && user.password === inputPassword) {
       return true;
     }
-
     return false;
+  }
 
+  getCurrentUser() {
+    // if current user is null fetch it from session
+    if (!this.currentUser) {
+      this.loadCurrentUserFromSession();
+    }
+    return this.currentUser;
+  }
+
+  setCurrentUser(user) {
+    this.currentUser = user;
+    this.updateSession();
   }
 
   updateSession() {
     window.sessionStorage.setItem('users', JSON.stringify(this.users));
+    window.sessionStorage.setItem('currentUser', JSON.stringify(this.currentUser));
   }
 
   loadUsersFromSession() {
     const storedUsers = window.sessionStorage.getItem('users');
     this.users = storedUsers ? JSON.parse(storedUsers) : [];
+    this.currentUser = window.sessionStorage.getItem('currentUser');
+    this.loadCurrentUserFromSession();
+  }
+
+  loadCurrentUserFromSession() {
+    const storedCurrentUser = window.sessionStorage.getItem('currentUser');
+    this.currentUser = storedCurrentUser ? JSON.parse(storedCurrentUser) : null;
   }
 }
 
